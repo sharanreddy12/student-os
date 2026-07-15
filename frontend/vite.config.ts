559660fd043845c1957d49693afa58bfa984864a
sharanreddy12 <durgamsharan@gmail.com>
2026-07-15 17:06:35 +0530

    update pjson vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackStartVite } from "@tanstack/react-start/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
-    TanStackStartVite({
+    tanstackStart({
       server: { entry: "server" },
     }),
     viteReact(),
